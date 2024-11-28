3f119cce06461af154690e85ae79c5ef41b14be0
Sebastian Vivo <svivo@uark.edu>
2024-11-27 21:56:50 -0600

    implement claudinary

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -82,7 +82,7 @@ export async function POST(req: Request) {
       });
     }
 
-    return NextResponse.json({ message: "OK", user: newUser });
+    return NextResponse.json({ status: 200, message: "OK", user: newUser });
   }
 
   // UPDATE
